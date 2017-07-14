d04a6f08910a6b16395d11b9ca00d18b461e03a6
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-07-15 00:30:43 +0530

    access sheets by heading

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -9,13 +9,13 @@ import {ContentService} from '../../services/content.service';
 export class AboutComponent implements OnInit {
   public json;
   public unique_identifier = '1mK2zvp6ouN5r7kh0StqkULr32l9MyO0suBpMJUL4QeM';
-  getHeroes() {
-     this.data.getCell(this.unique_identifier)
+  getData() {
+     this.data.getCellByTitle(this.unique_identifier)
       .subscribe(
         res => console.log(this.json = res));
   }
   constructor(private data:ContentService) {
-    this.getHeroes();
+    this.getData();
   }
   ngOnInit() {
   }
--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -1,13 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {SheetsService} from './sheets.service';
-
+let str = '{';
 @Injectable()
 export class ContentService {
-  getCell(unique_identifier):Observable<any>{
+public string;
+  getCellByTitle(unique_identifier): Observable<any> {
+
     return this.sheets.getData(unique_identifier)
-      .map(e => e.map(f=> f.gsx$b.$t));
+      .map(e => e.map(f => {
+        str = str.concat('"' + f.gsx$a.$t.toLowerCase() + '": "' + f.gsx$b.$t + '", ');
+        return f;
+      }))
+      .map(this.makeobject);
   }
-  constructor(public sheets: SheetsService) { }
+
+  private makeobject(): Observable<any> {
+    let str2 = str + '"end":"end"}';
+    return JSON.parse(str2) as Observable<any>;
+  }
+getCol(unique_identifier,colindex:string):Observable<any>{
+    let i=colindex;
+  return this.sheets.getData(unique_identifier)
+    .map(e => e.map(f =>f['gsx$'+i].$t));
+}
+  constructor(public sheets: SheetsService) {
+  }
 
 }
