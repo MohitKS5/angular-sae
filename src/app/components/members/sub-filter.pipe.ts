23e605d00bd05861840143dd80def92bb12e102f
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-09-24 01:40:34 +0530

    changes

--- src/app/components/members/sub-filter.pipe.ts
+++ src/app/components/members/sub-filter.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class SubFilterPipe implements PipeTransform {
   transform(array: any[], args: string): Array<any> {
-    console.log(args);
     return array.filter(item => item.subsystem == args);
   }
 }
--- src/assets/js/imageload.js
+++ src/assets/js/imageload.js
@@ -2,3 +2,4 @@ $(window).load(function(){
     $('img').not('img.nope').each(function(){
         $(this).attr('src', 'https://res.cloudinary.com/imagelibraryfsae/image/upload/v1498843998'+$(this).attr('alt')); });
 });
+//flexslider members page
